73175713dcb9dbd53502460f27d864fda9a4a2f9
Maya-99 <mayabouguandouz@gmail.com>
2019-09-20 15:19:37 +0200

    Create element for comments

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,6 +1,23 @@
 //Submit comment
 function submitComment(){
-    console.log('hello');
-}
+    const inputField = document.getElementById('inputText');  
+    const name = inputField.value;
+    const textArea = document.getElementById('msg');
+    const msg = textArea.value;
 
-submitComment();
+    //Wrap  user comments
+    console.log(name, msg);   
+
+    //make the space for user input
+    const commentBox = document.createElement('section');
+    const commentHead = document.createElement('h3');
+    const commentText = document.createElement('p');
+    commentHead.innerHTML = `${name} Said:`;
+    commentText.innerHTML = msg;
+    commentBox.classList.add(CommentBox);
+    commentBox.appendChild(commentHead);
+    commentBox.appendChild(commentText);
+    
+    console.log(commentBox);
+    
+}
